08f2fdafa472cb7562178190a10102b3b8d2f6e7
Sebastian Fekete <aight8@gmail.com>
2017-05-15 10:44:45 +0200

    Update API route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'api/get_newcomers', action: :get_newcomers, controller: 'api'
+  get 'api/get_newcomer_stories', action: :get_newcomers, controller: 'api'
 end
