411f2e9e8213c1312222f1da24b04c34c6485500
Sebastian Fekete <aight8@gmail.com>
2017-05-15 16:24:53 +0200

    Basic HN API response validation, use es_synchrony adapter

--- app/services/hacker_news_api.rb
+++ app/services/hacker_news_api.rb
@@ -1,7 +1,12 @@
-#require 'singleton'
-# include Singleton
-
 require 'faraday'
+
+class HackerNewsApiError < StandardError
+  attr_reader :response
+  def initialize(message, response = nil)
+    super(message)
+    @response = response
+  end
+end
 
 # Some endpoints of the HackerNews API with concurrency load support
 class HackerNewsApi
@@ -10,7 +15,8 @@ class HackerNewsApi
 
   def initialize
     @conn = Faraday.new(:url => @@api_base_url) do |faraday|
-      faraday.adapter :em_http
+      ##faraday.adapter :em_http
+      faraday.adapter :em_synchrony
       faraday.request :json
       faraday.response :json, :content_type => /\bjson$/
     end
@@ -19,25 +25,43 @@ class HackerNewsApi
   # Fetch one item
   def fetch_item(id)
     response = @conn.get "item/#{id}.json"
-    response.body
+    item = response.body
+
+    raise HackerNewsApiError.new "Response must be an item object.", response unless item.kind_of?(Hash) && item.has_key?('id')
+
+    Rails.logger.info "Fetch item succeed (id: #{item.id})"
+    item
   end
 
   # Fetch multiple items and return all items as array
   def fetch_multiple_items(ids)
     responses = []
+
     @conn.in_parallel do
       ids.each do |id|
-        responses.push @conn.get "item/#{id}.json"
+        # responses.push @conn.get "item/#{id}.json"
+        (@conn.get "item/#{id}.json").on_complete { |res| responses << res }
       end
     end
-    responses.each do |response|
-      response.body
+
+    items = responses.each do |response|
+      item = response.body
+      raise HackerNewsApiError.new "Response must be an item object.", response unless item.kind_of?(Hash) && item.has_key?('id')
+      item
     end
+
+    Rails.logger.info "Fetch multiple items succeed (count: #{responses.length})"
+    items
   end
 
   # Fetches the last 500 story ids
   def fetch_new_story_ids
     response = @conn.get 'newstories.json'
-    response.body
+    ids = response.body
+
+    raise HackerNewsApiError.new "Response must be an array.", response unless ids.kind_of?(Array)
+
+    Rails.logger.info "Fetch new story ids succeed (count: #{ids.length})"
+    ids
   end
 end
